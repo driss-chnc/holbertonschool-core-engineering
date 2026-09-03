41b93928a24ed75408e7e75d8b9f4e4f0d580b9f
driss-chnc <chaneac.driss@gmail.com>
2026-09-03 22:00:55 +0200

    Fix program to load variable

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 
-try:
-    from .variable_load_5 import a
-except ImportError:
-    from importlib import import_module
-
-    a = import_module("variable_load_5").a
+from variable_load_5 import a  # type: ignore[import-not-found]
 
 
 if __name__ == "__main__":
