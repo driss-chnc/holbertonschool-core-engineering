22a9d8a027cf90bf865b9da251deb50b350dff9f
driss-chnc <chaneac.driss@gmail.com>
2026-09-03 21:27:47 +0200

    Fix power function

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -4,7 +4,11 @@
 def pow(a, b):
     result = 1
 
-    for i in range(b):
-        result *= a
+    if b >= 0:
+        for i in range(b):
+            result *= a
+    else:
+        for i in range(-b):
+            result /= a
 
     return result
