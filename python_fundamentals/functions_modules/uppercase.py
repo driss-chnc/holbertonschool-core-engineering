535fdc87af9df8beaef9da18977b1c3beb0665bc
driss-chnc <chaneac.driss@gmail.com>
2026-09-03 15:59:31 +0200

    Add uppercase function

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+def uppercase(str):
+    for c in str:
+        if 'a' <= c <= 'z':
+            print(chr(ord(c) - 32), end="")
+        else:
+            print(c, end="")
+    print()
