d29fa9fd0955aecdd4c90d30473383c9327555cc
driss-chnc <chaneac.driss@gmail.com>
2026-09-13 12:30:14 +0200

    Add area and string representation to Rectangle

--- python_oop/inheritance/2-rectangle.py
+++ python_oop/inheritance/2-rectangle.py
@@ -2,7 +2,7 @@
 """Defines a Rectangle class."""
 
 
-BaseGeometry = __import__('0-base_geometry').BaseGeometry
+BaseGeometry = __import__('base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
